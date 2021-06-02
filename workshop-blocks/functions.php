61782aef163709658cf48fc8818d99824b1a1495
Daisy Olsen <daisy@daisyolsen.com>
2021-06-02 15:25:47 -0400

    Theme Updates

--- workshop-blocks/functions.php
+++ workshop-blocks/functions.php
@@ -3,7 +3,7 @@
 /**
  * Add theme supports.
  */
-function emptytheme_support()  {
+function workshop_support()  {
 
 	// Adding support for featured images.
 	add_theme_support( 'post-thumbnails' );
@@ -24,14 +24,14 @@ function emptytheme_support()  {
 	add_theme_support( 'custom-units' );
 }
 
-add_action( 'after_setup_theme', 'emptytheme_support' );
+add_action( 'after_setup_theme', 'workshop_support' );
 
 /**
  * Enqueue scripts and styles.
  */
-function emptytheme_scripts() {
+function workshop_scripts() {
 	// Enqueue theme stylesheet.
-	wp_enqueue_style( 'emptytheme-style', get_template_directory_uri() . '/style.css', array(), wp_get_theme()->get( 'Version' ) );
+	wp_enqueue_style( 'workshop-style', get_template_directory_uri() . '/style.css', array(), wp_get_theme()->get( 'Version' ) );
 }
 
-add_action( 'wp_enqueue_scripts', 'emptytheme_scripts' );
+add_action( 'wp_enqueue_scripts', 'workshop_scripts' );
